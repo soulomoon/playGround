5788567567155df3888d46cd5032692dcd7fed4b
soulomoon <fwy996602672@gmail.com>
2018-03-06 21:35:41 +0800

    Update formate

--- algorithm/bubblesort.py
+++ algorithm/bubblesort.py
@@ -7,17 +7,21 @@ def exchange(l):
     if len(l) > 1 and l[0] > l[1]:
         l[0], l[1] = l[1], l[0]
 
+
 def sround(l, *_):
     exchange(l)
     return l[0:1] + sround(l[1:]) if l else l
 
-bubblesort = lambda l: reduce(sround, range(len(l)), l)
+
+def bubblesort(l): return reduce(sround, range(len(l)), l)
+
 
 def bb_inplace(l):
     for i in range(len(l)):
         for j in range(len(l)-1):
             if l[j] > l[j+1]:
                 l[j], l[j+1] = l[j+1], l[j]
+
 
 if __name__ == "__main__":
     crt = list(range(5))
