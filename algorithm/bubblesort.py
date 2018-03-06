21b11c9bc22ed391b0cf646b5ee0776cf1681a35
soulomoon <fwy996602672@gmail.com>
2018-03-06 21:24:22 +0800

    Add bubble sort

---
+++ algorithm/bubblesort.py
@@ -0,0 +1,29 @@
+from operator import gt, le
+from functools import partial, reduce
+from itertools import permutations
+
+
+def exchange(l):
+    if len(l) > 1 and l[0] > l[1]:
+        l[0], l[1] = l[1], l[0]
+
+def sround(l, *_):
+    exchange(l)
+    return l[0:1] + sround(l[1:]) if l else l
+
+bubblesort = lambda l: reduce(sround, range(len(l)), l)
+
+def bb_inplace(l):
+    for i in range(len(l)):
+        for j in range(len(l)-1):
+            if l[j] > l[j+1]:
+                l[j], l[j+1] = l[j+1], l[j]
+
+if __name__ == "__main__":
+    crt = list(range(5))
+    cases = list(map(list, permutations(list(range(5)))))
+    for i in cases:
+        assert bubblesort(i) == crt
+        assert bb_inplace(i) == crt
+
+    print("test done total: {}".format(len(cases)))
--- algorithm/quicksort.py
+++ algorithm/quicksort.py
@@ -7,10 +7,12 @@ qs1 = lambda l: qs1([*filter(partial(gt, l[0]), l[1:])]) + l[0:1] + qs1([*filter
 qs2 = lambda l: qs2([i for i in l[1:] if i <= l[0]]) + l[0:1] + qs2([i for i in l[1:] if i > l[0]]) if l else []
 qs_l = [v for k, v in vars().items() if 'qs' in k]
 
-def test():
+if __name__ == "__main__":
     crt = list(range(5))
     cases = map(list, permutations(list(range(5))))
     for f in qs_l:
         for i in cases:
             assert f(i) == crt
             print("pass")
+
+    print("test done total: {}".format(len(cases)))
